71fead1a44ddb4cc31f37c39115acadf91712b17
jarif12538 <hasanjarif12538@gmail.com>
2026-02-17 22:58:47 +0600

    add initial variables and person map to start.dart

---
+++ start.dart
@@ -0,0 +1,10 @@
+String name = "JARIF";
+bool male = true;
+int age = 22;
+List<String> hobbies = ['Coding', 'Traveling', 'Cooking'];
+Map<String, dynamic> person = {
+  'name': name,
+  'male': male,
+  'age': age,
+  'hobbies': hobbies
+};
