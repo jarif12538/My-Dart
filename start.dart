e186b887387a1bfee148e7adad19f14ae86a88b1
jarif12538 <hasanjarif12538@gmail.com>
2026-02-17 23:12:24 +0600

    refactor main function to encapsulate variable declarations and logic

--- start.dart
+++ start.dart
@@ -1,14 +1,19 @@
-String name = "JARIF";
-bool male = true;
-int age = 22;
-List<String> hobbies = ['Coding', 'Traveling', 'Cooking'];
-Map<String, dynamic> person = {
-  'name': name,
-  'male': male,
-  'age': age,
-  'hobbies': hobbies
-};
-hobbies.addAll(["Reading", "Gaming","Football"]);
-hobbies.sort();
-bool val = hobbies.contains("Cooking");
-
+void main() {
+  String name = "JARIF";
+  bool male = true;
+  int age = 22;
+  List<String> hobbies = ['Coding', 'Traveling', 'Cooking'];
+  Map<String, dynamic> person = {
+    'name': name,
+    'male': male,
+    'age': age,
+    'hobbies': hobbies
+  };
+  hobbies.addAll(["Reading", "Gaming","Football"]);
+  hobbies.sort();
+  bool val = hobbies.contains("Cooking");
+  print(person);
+  print(person['age']);
+  print(person.keys.toList());
+  print(person.containsKey('name'));
+}
