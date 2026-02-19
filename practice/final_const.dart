752c998a81dba502b33e32b9d3a45ca28f690873
jarif12538 <hasanjarif12538@gmail.com>
2026-02-19 23:18:46 +0600

    update comments in final_const.dart for clarity on final and const variables

--- practice/final_const.dart
+++ practice/final_const.dart
@@ -3,6 +3,7 @@ void main() {
   final String name = "JARIF";
   final bool male = true;
   //A const variable is a compile-time constant, which means its value must be known at compile time and cannot be changed at runtime.
+  //A const variable is implicitly final, which means it cannot be reassigned after it has been initialized.
   const int age = 22;
   final List<String> hobbies = ['Coding', 'Traveling', 'Cooking'];
   Map<String, dynamic> person = {
@@ -13,12 +14,12 @@ void main() {
   };
   print(person);
 }
+//In Dart, a final variable can be assigned a value only once, and it can be assigned at runtime. A const variable, on the other hand, must be assigned a value at compile time and cannot be changed at runtime. Additionally, a const variable is implicitly final, meaning it cannot be reassigned after it has been initialized.
 //static variables are shared among all instances of a class, while instance variables are unique to each instance of the class. Static variables belong to the class itself, while instance variables belong to individual objects created from the class.
 class MYClass {
   final String name = "JARIF";
   static int age = 25;
 }
-
 void testClass() {
   MYClass myClass = MYClass();
   print(myClass.name);
